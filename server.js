2a9cf2afa9febeb6d37061a68217dd1ec875a78c
Роман Шухевич <romanberezdetskyi@MacBook-Pro-Roman.local>
2021-12-15 13:40:10 +0200

    basic logic added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Bot is still running!');
+});
+
 app.post('/', async (req, res) => {
   const body = req.body;
   console.log(body);
